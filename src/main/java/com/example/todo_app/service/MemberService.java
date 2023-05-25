fe8da576d90ab87628d821c62d148e210abba257
kimskyyyy <hhhhh823@naver.com>
2023-05-25 14:10:48 +0900

    create MemberService

--- src/main/java/com/example/todo_app/service/MemberService.java
+++ src/main/java/com/example/todo_app/service/MemberService.java
@@ -1,2 +1,33 @@
-package com.example.todo_app.service;public class MemberService {
+package com.example.todo_app.service;
+
+import com.example.todo_app.domain.MemberEntity;
+import com.example.todo_app.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    // 회원 생성
+    public MemberEntity create(final MemberEntity memberEntity) {
+        if(memberEntity == null || memberEntity.getEmail() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String email = memberEntity.getEmail();
+        if(memberRepository.existsByEmail(email)) {
+            log.warn("Email already exists {}", email);
+            throw  new RuntimeException("Email already exists");
+        }
+
+        return memberRepository.save(memberEntity);
+    }
+
+    public MemberEntity getByCredentials(final String email, final String password) {
+        return memberRepository.findByEmailAndPassword(email, password);
+    }
 }
