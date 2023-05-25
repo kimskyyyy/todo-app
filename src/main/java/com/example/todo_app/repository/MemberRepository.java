306f4d9de8055a35bc05ed6e1450903311958205
kimskyyyy <hhhhh823@naver.com>
2023-05-25 14:10:11 +0900

    create MemberRepository

--- src/main/java/com/example/todo_app/repository/MemberRepository.java
+++ src/main/java/com/example/todo_app/repository/MemberRepository.java
@@ -1,2 +1,16 @@
-package com.example.todo_app.repository;public interface MemberRepository {
+package com.example.todo_app.repository;
+
+import com.example.todo_app.domain.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, String> {
+
+    MemberEntity findByEmail(String email);
+    Boolean existsByEmail(String email);
+    MemberEntity findByEmailAndPassword(String email, String password);
+
+
+
 }
