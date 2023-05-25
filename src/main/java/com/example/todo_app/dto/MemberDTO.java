29f88edcafc676436a016161416b38de5c040582
kimskyyyy <hhhhh823@naver.com>
2023-05-25 14:09:02 +0900

    create MemberDTO

--- src/main/java/com/example/todo_app/dto/MemberDTO.java
+++ src/main/java/com/example/todo_app/dto/MemberDTO.java
@@ -1,2 +1,18 @@
-package com.example.todo_app.dto;public class MemberDTO {
+package com.example.todo_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+    private String token;
+    private String email;
+    private String username;
+    private String password;
+    private String id;
 }
